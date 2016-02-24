00bcf7dbabdc0c232a30edc4abbbf7d8bc6a763d
Handle <857571554@qq.com>
2016-02-25 02:01:38 +0800

    Added files via upload
    V1.0 任何需要的地方直接引用js即可

---
+++ json.php
@@ -0,0 +1,1 @@
+<?php $source =  $_GET['source'];$url_long =  $_GET['url_long'];$url = "http://api.weibo.com/2/short_url/shorten.json?";$wtf = array('source'=> $source,'url_long' => $url_long,);echo file_get_contents($url.http_build_query($wtf)); ?>
