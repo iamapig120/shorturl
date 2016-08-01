7413220e4a6dcd8361ef273e8e50fa03879120a3
Handle <857571554@qq.com>
2016-08-01 21:22:35 +0800

    1.2版本的新浪短址，修正了一处显示问题

---
+++ shorturl-1.2.js
@@ -0,0 +1,15 @@
+document.write('<form name="input" class="su_inp" >请输入长网址：<br><input style="width: 100%;" placeholder="例如https://www.bysb.net/" id="long" name="long" class="su_url" ><br><div id="link" class="su_link" ><p>生成的短网址：</p></div><br><a class="su_btn" href="javascript:shorturl()">点击这里生成</a><br><a class="su_edi" href="https://www.bysb.net/" target="_blank">作者：手柄君</a></form>');//生成整个表单
+function shorturl() {
+	var long = document.input.long.value;//获取长网址 
+	var long=long.replace(/\&/g,'%26');//对神奇的&进行转义
+	var long=long.replace(/\#/g,'%23');//对神奇的#进行转义
+	var url = "/json.php";//填写基于根的json.php地址
+	var app_key = "2849184197";//app key来自倒霉的渣浪
+	var cmd2 = url + "?source=" + app_key + "&url_long=" + long; //生成请求地址
+	xmlhttp=new XMLHttpRequest();//建立ajax对象
+	xmlhttp.open("GET",cmd2,false);//同步，GET
+	xmlhttp.send();//发送GET请求
+	var fuckjs = new Function("return" + xmlhttp.responseText)();//将返回数据视作对象
+	var fuckjson = fuckjs.urls[0].url_short;//提取短网址
+	document.getElementById("link").innerHTML="<p>生成的短网址："+fuckjson+"</p>";//输出短网址
+}; 
